713fa8c10fd6d39715ac1e2b5334f9556c256781
wpcampbell <77030540+wpcampbell@users.noreply.github.com>
2021-06-09 18:24:07 -0500

    change

--- js/main.js
+++ js/main.js
@@ -55,4 +55,17 @@ var geojsonFeature = {
         "coordinates": [-104.99404, 39.75621]
     }
 };
-
+//tutorial 2
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+L.geoJSON(geojsonFeature).addTo(map);
