a79a35ae1398c01a0f7f7c8c940e295972943dfa
wpcampbell <77030540+wpcampbell@users.noreply.github.com>
2021-06-26 17:25:30 -0500

    6-26-21

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 /*Stylesheet by Will P. Campbell,2021*/
 
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery   <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoid3BjYW1wYmVsbCIsImEiOiJja2prZGxiZjQwN2NsMzBuczJ2Y2FhNmg5In0.cMYrG4f2ipHv9enkr48czQ'
+}).addTo(mymap);
--- js/tutorials.js
+++ js/tutorials.js
@@ -6,7 +6,7 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery   <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox.satellite',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoid3BjYW1wYmVsbCIsImEiOiJja2prZGxiZjQwN2NsMzBuczJ2Y2FhNmg5In0.cMYrG4f2ipHv9enkr48czQ'
