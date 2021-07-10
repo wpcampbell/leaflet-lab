cd7d14c4c949f8edb276a64c642b31a0d221ff96
wpcampbell <77030540+wpcampbell@users.noreply.github.com>
2021-07-09 19:13:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@
 
 var mymap = L.map('mapid').setView([44.5,-90], 6.75);
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery   <a href="http://mapbox.com">Mapbox</a>',
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: 'abcd',
+    minZoom: 0,
     maxZoom: 18,
-    id: 'mapbox.mapbox-terrain-v2',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoid3BjYW1wYmVsbCIsImEiOiJja3Fza2RxMnMxeTh2MnhzYmpxbDV6ZTRkIn0.UQhYco5_eD8IXTwwrULz4A'
+    ext: 'png'
 }).addTo(mymap);
