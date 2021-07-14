f9918ef28024b80b6c9ebb48d5cd784a7ae26473
wpcampbell <77030540+wpcampbell@users.noreply.github.com>
2021-07-14 13:44:09 -0500

    7-14

--- js/geojson.js
+++
@@ -1,7 +0,0 @@
-$.ajax(data/Glacier_Toe_11000.json, {
-    dataType: "json",
-    success: function(response) {
-
-        L.geoJson(response).addTo(mymap);
-    }
-})
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*Stylesheet by Will P. Campbell,2021*/
 
-var mymap = L.map('mapid').setView([44.5,-90], 6.75);
+var mymap = L.map('mapid').setView([44.5,-90], 6);
 
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
